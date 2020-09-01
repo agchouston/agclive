18ab0ae6d597c1950d11eaa952f1bf56c488e8b5
AGC Houston <39772386+agchouston@users.noreply.github.com>
2020-08-31 19:40:45 -0500

    Config Update

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_JTi25cDVB',
-        userPoolClientId: '5qi4adsulhj6c35qgngict3car',
-        region: 'eu-west-1'
+        userPoolId: 'us-east-2_mCErLYrQc',
+        userPoolClientId: '22j6cqvcvcc118nh5po4bct2c7',
+        region: 'us-east-2'
     },
     api: {
-        invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
+        invokeUrl: ''
     }
 };
